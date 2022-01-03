016c4e79b50b1261b0729597b2993c8b9b83acf4
Ankur Vatsa <ankur.vatsa@gmail.com>
2022-01-04 03:39:47 +0530

    [#2]: Implemented transfer tokens

--- contracts/BhktToken.sol
+++ contracts/BhktToken.sol
@@ -2,13 +2,34 @@
 pragma solidity ^0.8.10;
 
 contract BhktToken {
-    // Constructor 
-    // Set the total number of tokens in circulation
-    // Read the total number of tokens
-
+    string public name = "BHKT Token";
+    string public symbol = "BHKT";
+    string public standard = "BHKT Token v1.0";
     uint256 public totalSupply;
 
+    event Transfer(
+        address indexed _from, 
+        address indexed _to, 
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+
     constructor (uint256 _totalSupply) {
+        balanceOf[msg.sender] = _totalSupply;
         totalSupply = _totalSupply;
     }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        // raise the event now
+        emit Transfer(msg.sender, _to, _value);
+
+        // Return the bool value
+        return true;
+    }
 }
+
--- test/BhktToken.js
+++ test/BhktToken.js
@@ -1,11 +1,64 @@
 var BhktToken = artifacts.require("./BhktToken.sol");
 
 contract('BhktToken', function(accounts) {
-    it('Sets totalSupply upon deployment', function() {
+    var tokenInstance;
+
+    it('Initializes the contract with the correct values', function() {
         return BhktToken.deployed().then(function(instance) {
-            return instance.totalSupply();
-        }).then(function(totalSupply) {
-            assert.equal(totalSupply.toNumber(), 1000000, "Total supply is not 1,000,000");
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(name) {
+            assert.equal(name, 'BHKT Token', 'has the correct name');
+            return tokenInstance.symbol();
+        }).then(function(symbol) {
+            assert.equal(symbol, 'BHKT', 'has the correct symbol');
+            return tokenInstance.standard();
+        }).then(function(standard) {
+            return assert.equal(standard, "BHKT Token v1.0", 'has the correct standard');
         });
-    })
-})
+    });
+
+    it('Allocates initial supply upon deployment', function() {
+        return BhktToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.totalSupply();
+        }).then(function(totalSupply) {
+            assert.equal(totalSupply.toNumber(), 1000000, "Sets the total supply to 1,000,000");
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(adminBalance) {
+            assert.equal(adminBalance.toNumber(), 1000000, "It allocates initial supply to the admin account");
+        });
+    });
+
+    it('transfers token ownership', function() {
+        return BhktToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            // We use transfer.call() as we don't want a transaction to be created, but only to inspect
+            return tokenInstance.transfer.call(accounts[1], 9000000000, {from: accounts[0]});
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+            // Again, use transfer.call() as we don't want a transaction to be created
+            // we only want to inspect the return from the function, is true of false
+            return tokenInstance.transfer.call(accounts[1], 250000, {from: accounts[0]});
+        }).then(function(success) {
+            assert.equal(success, true, "it returns true");
+            // Without .call() transfer() creates a transaction, unlike transfer.call()
+            return tokenInstance.transfer(accounts[1], 250000, {from: accounts[0]});
+        }).then(function(receipt) {
+            // We can test for events by looking at transaction receipt
+            // Event information is in receipt.logs[n]
+            assert.equal(receipt.logs.length, 1, "triggers one event");
+            // As there is only 1 event, we use the first index - hardcoded
+            assert.equal(receipt.logs[0].event, "Transfer", "should be the transfer event");
+            assert.equal(receipt.logs[0].args._from, accounts[0], "logs the account the tokens are transferred from");
+            assert.equal(receipt.logs[0].args._to, accounts[1], "logs the account the tokens are transferred to");
+            assert.equal(receipt.logs[0].args._value, 250000, "logs the transfer amount");
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 250000, "adds the amount to the receiving account");
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 750000, "deducts the amount from the sending account");
+        });
+    });
+});
