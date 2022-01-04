8c52a2ca68852b2f69b269b16a4f9252095fba1d
Ankur Vatsa <ankur.vatsa@gmail.com>
2022-01-04 14:45:29 +0530

    [#4]: Getting started with Token Sale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const BhktToken = artifacts.require("./BhktToken.sol");
+const BhktTokenSale = artifacts.require("./BhktTokenSale.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(BhktToken, 1000000);
+  deployer.deploy(BhktToken, 1000000).then(function () {
+    var tokenPrice = 1000000000000000; // in wei = 0.001 ETH
+    return deployer.deploy(BhktTokenSale, BhktToken.address, tokenPrice);
+  });
 };
