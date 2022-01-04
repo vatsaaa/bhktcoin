386ec41e95cd4a807f9fdca8fef15b7b5aa2b28f
Ankur Vatsa <ankur.vatsa@gmail.com>
2022-01-04 11:41:14 +0530

    [#3]: Delegated transfer

--- contracts/BhktToken.sol
+++ contracts/BhktToken.sol
@@ -13,7 +13,17 @@ contract BhktToken {
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner, 
+        address indexed _spender, 
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+
+    // One account can approve multiple 3rd parties to sell / spend tokens
+    // Hence, we need a mapping of "approver" to all the "spenders" and how much are they approved to spend
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor (uint256 _totalSupply) {
         balanceOf[msg.sender] = _totalSupply;
@@ -29,6 +39,43 @@ contract BhktToken {
         emit Transfer(msg.sender, _to, _value);
 
         // Return the bool value
+        return true;
+    }
+
+    // Delegated transfers
+
+    // allows another account to spend the tokens on behalf of the sender
+    // used in a scenario where they approve the exchange to sell X tokens 
+    // thus the exchane can transfer X tokens on your behalf
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        // msg.sender allows _spender to spend _value tokens
+        allowance[msg.sender][_spender] = _value;
+
+        // raise the event now
+        emit Approval(msg.sender, _spender, _value);
+
+        // Return the bool value
+        return true;
+    }
+
+    // after approve() of certain amount, transferFrom() actually handles that transfer
+    // and from there the exchange (or another approved 3rd party) can execute the transfer
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        // Require that _from has enough tokens to transfer
+        require(_value <= balanceOf[_from]);
+
+        // Require allowance is big enough to cover transfer
+        require(_value <= allowance[_from][msg.sender]);
+
+        // Balance of _from account is reduced by _value and _to account is increased by _value
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        // Update the allowance
+        allowance[_from][msg.sender] -= _value;
+
+        // Transfer event
+        emit Transfer(_from, _to, _value);
         return true;
     }
 }
--- test/BhktToken.js
+++ test/BhktToken.js
@@ -61,4 +61,74 @@ contract('BhktToken', function(accounts) {
             assert.equal(balance.toNumber(), 750000, "deducts the amount from the sending account");
         });
     });
+
+    it('approves tokens for delegated transfer', function() {
+        return BhktToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            // Simply calls approve without writing to the blockchain
+            return tokenInstance.approve.call(accounts[1], 100);
+        }).then(function(success) {
+            assert(success, true, 'it returns true');
+            // Here we do want to create the transaction
+            return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+        }).then(function(receipt) {
+            // We can test for events by looking at transaction receipt
+            // Event information is in receipt.logs[n]
+            assert.equal(receipt.logs.length, 1, "triggers one event");
+            // As there is only 1 event, we use the first index - hardcoded
+            assert.equal(receipt.logs[0].event, "Approval", "should be the transfer event");
+            assert.equal(receipt.logs[0].args._owner, accounts[0], "logs the account the tokens are authorised by");
+            assert.equal(receipt.logs[0].args._spender, accounts[1], "logs the account the tokens are authorised to");
+            assert.equal(receipt.logs[0].args._value.toNumber(), 100, "logs the transfer amount");
+            return tokenInstance.allowance(accounts[0], accounts[1]);
+        }).then(function(allowance) {
+            assert.equal(allowance.toNumber(), 100, "stores the allowance for delegated transfer");
+        });
+    });
+
+    it('handles delegated token transfers', function() {
+        let fromAccount = accounts[2];
+        let toAccount = accounts[3];
+        let spendingAccount = accounts[4];
+
+        return BhktToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            // Transfer some tokens to fromAccount
+            return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
+        }).then(function(receipt) {
+            // Approve spendingAccount to spend 10 tokens from fromAccount);
+            return tokenInstance.approve(spendingAccount, 10, { from: fromAccount });
+        }).then(function(receipt) {
+            // Try transferring something larger than the sender's balance
+            return tokenInstance.transferFrom(fromAccount, toAccount, 9999, { from: spendingAccount });
+        }).then(assert.fail).catch(function(error) {
+            // check for error to contain string 'revert'
+            assert(error.message.indexOf('revert') >= 0, "cannot transfer value larger than balance");
+            // Try transferring something larger than the approved amount
+            return tokenInstance.transferFrom(fromAccount, toAccount, 20, { from: spendingAccount });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, "cannot transfer value larger than approved amount");
+            return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount });
+        }).then(function(success) {
+            assert.equal(success, true, "it returns true");
+            return tokenInstance.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount });
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, "triggers one event");
+            assert.equal(receipt.logs[0].event, "Transfer", "should be the transfer event");
+            assert.equal(receipt.logs[0].args._from, fromAccount, "logs the account the tokens are transferred from");
+            assert.equal(receipt.logs[0].args._to, toAccount, "logs the account the tokens are transferred to");
+            assert.equal(receipt.logs[0].args._value, 10, "logs the transfer amount");
+            return tokenInstance.balanceOf(fromAccount);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 90, "deducts the amount from the sending account");
+            return tokenInstance.balanceOf(toAccount);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 10, "adds the amount to the receiving account");
+            // spendingAccount is allowed to spend some amount from fromAccount
+            return tokenInstance.allowance(fromAccount, spendingAccount);
+        }).then(function(allowance) {
+            // since the allowance was 10 tokens, we would not be left with anymore tokens to spend
+            assert.equal(allowance.toNumber(), 0, "deducts the amount from the allowance");
+        });
+    });
 });
